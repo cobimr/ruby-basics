2b97d81dbe20dd76ceffff3ed044a21ff8997e84
Jacobo Martinez <jacobojavier98@gmail.com>
2018-09-11 09:43:23 -0400

    Added numbers excercises

---
+++ Basics/excercise/numbers.rb
@@ -0,0 +1,11 @@
+# how many hours are in a year?
+puts 365 * 24 
+
+# how many minutes are in a decade? 
+puts 365 * 24 * 60 * 10
+
+# how many seconds old are you? 
+puts  365 * 24 * 60 * 60 * 19
+
+# If I am 1298 million seconds old, how old am I?
+puts 1298000000 / (365 * 24 * 60 * 60)
